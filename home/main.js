dd8c37c4b9165b0e8e1399a884b781cf2d9da268
Daus290 <afrds290@gmail.com>
2025-04-01 13:13:42 +0800

    perfected footer

--- home/main.js
+++ home/main.js
@@ -1,15 +1,25 @@
 // navbar berubah warna jadi putih polos ketika di scroll ke bawah
 $(document).ready(function() {
+    let lastScrollTop = 0;
+
     $(window).scroll(function() {
-        if ($(this).scrollTop() > 50) {
-            $("#header").addClass("scrolled");
-        } else {
-            $("#header").removeClass("scrolled");
+        let scrollTop = $(this).scrollTop();
+
+        if (scrollTop > 50) {
+            $("#header").addClass("scrolled").removeClass("scrolled-remove");
+        } else if (scrollTop < lastScrollTop) {
+            $("#header").addClass("scrolled-remove").removeClass("scrolled");
         }
+
+        lastScrollTop = scrollTop;
     });
 });
+
 
 // tahun copyright otomatis
 $(document).ready(function() {
     $("#year").text(new Date().getFullYear());
 });
+
+
+//observer viewport scroll
